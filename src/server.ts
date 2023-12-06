6b297f81b4e0a7a32e292b506eb65fd0365eda1c
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 02:30:43 +0000

    17

--- src/server.ts
+++ src/server.ts
@@ -18,32 +18,7 @@ const app = Express();
 app.use(cookieParser());
 
 // Enable CORS with options for your frontend domain
-const corsOptions = {
-    origin: 'https://shiftmate-frontend.netlify.app',
-    credentials: true,
-};
-
-// Set middleware of CORS 
-// app.use((req, res, next) => {
-//     res.setHeader(
-//         "Access-Control-Allow-Origin",
-//         "https://shiftmate-frontend.netlify.app"
-//     );
-//     res.setHeader(
-//         "Access-Control-Allow-Methods",
-//         "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//     );
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     res.setHeader("Access-Control-Allow-Private-Network", "true");
-//     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-//     res.setHeader("Access-Control-Max-Age", "7200");
-
-//     next();
-// });
+app.use(cors());
 
 // Allow for request body to be accessed
 app.use(Express.json());
