df8e2d400fdc90a0767e59b9e154b3c1d215df50
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 13:41:32 +0000

    26

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,14 @@ dotenv.config();
 // Express app
 const app = Express();
 
-app.use(cors());
+app.use(cors(
+    {
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }
+));
 
 app.options('*', cors());
 
