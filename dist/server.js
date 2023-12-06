ff469bd995a1dd96bd0f7d73cf803a2bb2f4493a
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 17:28:19 +0000

    changed allowed oriign to https://shiftmate-frontend.vercel.app

--- dist/server.js
+++ dist/server.js
@@ -22,7 +22,7 @@ app.use((0, cors_1.default)({
 }));
 app.use((req, res, next) => {
     console.log('CORS middleware - setting headers');
-    res.header('Access-Control-Allow-Origin', ['https://shiftmate-frontend.netlify.app', 'https://shiftmate-frontend.vercel.app']);
+    res.header('Access-Control-Allow-Origin', 'https://shiftmate-frontend.vercel.app');
     res.header('Access-Control-Allow-Methods', 'POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
     console.log('CORS middleware - setting headers');
-    res.header('Access-Control-Allow-Origin', ['https://shiftmate-frontend.netlify.app', 'https://shiftmate-frontend.vercel.app']);
+    res.header('Access-Control-Allow-Origin', 'https://shiftmate-frontend.vercel.app');
     res.header('Access-Control-Allow-Methods', 'POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
