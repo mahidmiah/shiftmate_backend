eaf4b7a249431129d2d93efd4966d341e14ca189
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 00:35:44 +0000

    4

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 // Use CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Origin', 'https://shiftmate-frontend-mahid-miahs-projects.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
