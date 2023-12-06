afd683731d8e97e474741bd1c5ea6b35aa9b6528
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 00:26:38 +0000

    2

--- src/server.ts
+++ src/server.ts
@@ -23,12 +23,11 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-});
-
-// Handle preflight requests
-app.options('*', (req, res) => {
-    res.status(200).send();
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 // Allow for request body to be accessed
