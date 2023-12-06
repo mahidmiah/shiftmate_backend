420f3e4516a886c7e82e353d225f15a6bdf066eb
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 00:11:25 +0000

    added origin back

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 // Use CORS
 app.use(
     cors({
-        origin: '*',
+        origin: 'https://shiftmate-frontend-mahid-miahs-projects.vercel.app',
         credentials: true,
     })
 );
