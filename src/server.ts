59f46409978b7e2fdcac8edfa8308e183cc45074
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 13:34:23 +0000

    25

--- src/server.ts
+++ src/server.ts
@@ -13,11 +13,7 @@ dotenv.config();
 // Express app
 const app = Express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 app.options('*', cors());
 
