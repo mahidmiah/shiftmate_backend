cdc2a3da9515b39cc67d1daa3a80c148006b4849
Mahid Miah <miah.mahid@outlook.com>
2023-12-07 01:31:51 +0000

    secure: false,

--- dist/controllers/businessController.js
+++ dist/controllers/businessController.js
@@ -33,7 +33,7 @@ const loginUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             httpOnly: false,
             maxAge: 3 * 24 * 60 * 60 * 1000,
             sameSite: 'none',
-            secure: true, // Ensure it's served over HTTPS
+            secure: false, // Ensure it's served over HTTPS
         }).json({ email, token });
     }
     catch (error) {
