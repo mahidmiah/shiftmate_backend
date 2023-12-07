72f44a5ffa93e173599b6701923cac2141f58ed9
Mahid Miah <miah.mahid@outlook.com>
2023-12-07 01:37:24 +0000

    secure: true, // Ensure it's served over HTTPS

--- src/controllers/businessController.ts
+++ src/controllers/businessController.ts
@@ -23,7 +23,7 @@ export const loginUser = async (req: Request, res:Response) => {
             httpOnly: false,
             maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days (in milliseconds)
             sameSite: 'none', 
-            secure: false, // Ensure it's served over HTTPS
+            secure: true, // Ensure it's served over HTTPS
         }).json({email, token});
     } catch (error: any) {
         res.status(400).json({error: error.message});
